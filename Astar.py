9754fbfd26f2ecd9a53f87ecaf3a9636e638bf92
Hassan Mehmood <hmehmood@student.oulu.fi>
2018-04-10 15:19:15 +0300

    Completed algorithm
    Request contains completed algorithm and changesets

--- Astar.py
+++ Astar.py
@@ -7,112 +7,240 @@ from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Odometry
 import numpy as np
 import math
-# neighbour = []
+
 class goToPose():
-	#topic information and algorithm
-	#neighbour = []
-    def __init__(self):
-    	rospy.init_node('mynode')
-        rospy.wait_for_message('/map',OccupancyGrid)
-        rospy.Subscriber('/map',OccupancyGrid,self.getMap)
-        rospy.spin()
-#map information and conversion (grid coordinates)
-    def getMap(self,dataMsg):
-    	emptyList = [] 
-    	idGlobal = 0
-    	mapDataCounter = 0
-    	self.data = dataMsg.data
-    	#find me (map coordinates)
-    	for i in range(-10,374):
-    		for j in range(-10,374):
-				newNode = node(0,0,0,0,0,0,0) 
-				newNode.ids = idGlobal                                                                                                                                              
-				newNode.status = self.data[mapDataCounter]
-				newNode.x = j
-				newNode.y = i
-				
-				emptyList.append(newNode)
-				mapDataCounter = mapDataCounter+1
-				idGlobal = idGlobal+1
-		#print len(emptyList)
-	startNode = emptyList[407]
-	print startNode.x
-	print startNode.y
-# help me search my neighbours
-	goalNode = emptyList[89]
-	neighbour = []
-	newNode1 = node(0,0,0,0,0,0,0) 
-	currentNode = startNode
-	for x in emptyList:
-		
-		if (x.x == (currentNode.x-1) and x.y == currentNode.y):
-			newNode1 = x
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x+1) and x.y == currentNode.y):
-			newNode1 = x
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x) and x.y == currentNode.y+1):
-			newNode1 = x
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x) and x.y == currentNode.y-1):
-			newNode1 = x 
-			print newNode1.x
-			print newNode1.y
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x+1) and x.y == currentNode.y+1):
-			newNode1 = x
-			print newNode1.x
-			print newNode1.y
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x-1) and x.y == currentNode.y+1):
-			newNode1 = x
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x+1) and x.y == currentNode.y-1):
-			newNode1 = x
-			neighbour.append(newNode1)
-
-		if (x.x == (currentNode.x-1) and x.y == currentNode.y-1):
-			newNode1 = x
-			neighbour.append(newNode1)
-			print str(len(neighbour)) + 'inin'
-
-	print str(len(neighbour)) +'xyz'
-	o=0
-	def heuristicsCostEst(self, xDest, yDest):
-#Euclidean for intelligent distance guess	
-	    xd = xDest - self.x
-	    yd = yDest - self.y
-	    print xd + yd
-	    d = math.sqrt(xd * xd + yd * yd)
+	global smallestScore
+	global findNeighbours
+	global checkOpenSet
+	global calculateCosts
+	global estimate
+	global checkClosedSet
+	global reConstruct
+	#global closedSet
+	global currentNode
+	currentNode=node(-1,-1,-1,-1,-1,-1,-1,-1)
+	global goalNode
+	goalNode =node(-1,-1,-1,-1,-1,-1,-1,-1)
+	global startNode
+	startNode =node(-1,-1,-1,-1,-1,-1,-1,-1)
+	#initializing rostopic data /map
+	def __init__(self):
+		global cameFrom
+		cameFrom = []
+		global openSet
+		openSet = set()
+		global closedSet
+		closedSet = set()
+		global neighbours
+		neighbours=[]
+		global obstStatus
+		obstStatus = []
+		rospy.init_node('mynode')
+		rospy.wait_for_message('/map',OccupancyGrid)
+		rospy.Subscriber('/map',OccupancyGrid,self.updateMap)
+		rospy.spin()
+		#calculate cost to estimate heuristics
+	def estimate(self, xDest, yDest):
+	    xD = xDest - self.x
+	    yD = yDest - self.y
+	    d = math.sqrt(xD * xD + yD * yD)
 	    return (d)
+	    #Euclidean theorem for intelligent distance guess
+	def calculateCosts(currentNode):
+		movementCost= [10, 10, 10, 10, 14, 14, 14, 14] 
+		newNode=node(-1,-1,-1,-1,-1,-1,-1,-1) # new node initialized to append neighbours
+		o=0
+		for val in neighbours:
+			val.g=currentNode.g + movementCost[o]
+			val.h=estimate(val,goalNode.x,goalNode.y)
+			val.f=val.g+val.h
+			val.parent=currentNode.ids
+			o=o+1
+			# finding neighbours from map
+	def findNeighbours(currentNode):
+		# Clear neighbours list before iteration
+		if neighbours:
+			del neighbours[:]
+		for x in cameFrom:
 
-	movementCost = [10, 10, 10, 10, 14, 14, 14, 14 ]
-	print str(neighbour) + 'abc'
-	for val in neighbour:
-		val.g = currentNode.g + movementCost[o]
-		val.h = heuristicsCostEst(val,60,34)
-		val.f = val.g + val.h
-		val.parent = currentNode.ids
-		o = o + 1
-	print neighbour[4].x
-	print neighbour[4].h
+			if (x.x==(currentNode.x-1) and x.y==currentNode.y and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
 
-		
-        
+			if (x.x==(currentNode.x+1) and x.y==currentNode.y and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
 
+			if (x.x==(currentNode.x) and x.y==currentNode.y+1 and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
 
+			if (x.x==(currentNode.x) and x.y==currentNode.y-1 and x.status!=100):
+				newNode=x
+				print str(newNode.x)+ ' / Neighbours'
+				print str(newNode.y)+ ' / Neighbours'
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
+
+			if (x.x==(currentNode.x+1) and x.y==currentNode.y+1 and x.status!=100):
+				newNode=x
+				print str(newNode.x) + ' / Neighbours'
+				print str(newNode.y) + ' / Neighbours'
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
+				
+
+			if (x.x==(currentNode.x-1) and x.y==currentNode.y+1 and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
+
+			if (x.x==(currentNode.x+1) and x.y==currentNode.y-1 and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
+
+			if (x.x==(currentNode.x-1) and x.y==currentNode.y-1 and x.status!=100):
+				newNode=x
+				neighbours.append(newNode)
+				fchStatus = x.status
+				obstStatus.append(fchStatus)
+		print str(obstStatus) + '/Check status of obstacles on the path'
+		#check open set items
+	def checkOpenSet(x):
+		i=x
+		flag=True
+		for j in openSet:
+			if (i.ids==j.ids and i.f < j.f):
+				j.g=i.g
+				j.f=i.g+i.h
+				j.parent=i.parent
+				flag=False
+				break
+			if(i.ids==j.ids and i.f >= j.f):
+				flag=False
+				break
+		if (flag==True):
+			#print i.ids
+			openSet.add(i)
+		return flag
+		# check closed set items
+	def checkClosedSet(x):
+		i=x	
+		flag1=True
+		for j in closedSet:
+			if (i.ids==j.ids):
+				flag1=False
+		return flag1
+
+	def updateMap(self,mapDataMsg):
+		idGlobal=0
+		c=0
+		# retrieve map data dynamically (changeset)
+		self.data=mapDataMsg.data
+		self.width=mapDataMsg.info.width
+		self.height=mapDataMsg.info.height
+		self.originX=int(mapDataMsg.info.origin.position.x)
+		self.originY=int(mapDataMsg.info.origin.position.y)
+		print str(self.originX) + '/ Origin X'
+		print str(self.originY) + '/ Origin Y'
+		self.widthNew =self.width + self.originX
+		self.HeightNew =self.height + self.originY
+		for i in range(self.originY,self.HeightNew):
+			for j in range(self.originX,self.widthNew):
+				n=node(-1,-1,-1,-1,-1,-1,-1,-1)
+				n.ids=idGlobal
+				n.status=self.data[c]
+				n.x=j
+				n.y=i
+				cameFrom.append(n) # append map data to list
+				c=c+1
+				idGlobal=idGlobal+1
+		goal=0
+		goalNode=cameFrom[400]
+		startNode=cameFrom[2]
+		print str(startNode.x) + '/ Starting node X'
+		print str (startNode.y) + '/ Starting node Y'
+		# check for obstacles i.e. start node and goal node is not an obstacle
+		if(startNode.status != 100 and goalNode.status != 100):
+			print str(startNode.status) + 'Obstacle status before add objects to sets Start Node'
+			print str(goalNode.status) + 'Obstacle status before add objects to sets Goal Node'
+			startNode.g=0
+			xd = goalNode.x - startNode.x
+			yd = goalNode.y - startNode.y
+			startNode.h= math.sqrt(xd * xd + yd * yd)
+			startNode.f=startNode.g + startNode.h
+		else:
+			print "Choose start and goal again"
+		openSet.add(startNode)
+		#print list(openSet)
+		currentNode=startNode
+		# run, flow of algorithm
+		while(goal ==0):
+			if (len(openSet)!=0):
+				smallest = smallestScore()
+				currentNode=smallest
+				openSet.remove(smallest)
+				closedSet.add(smallest)
+				findNeighbours(currentNode)
+				calculateCosts(currentNode)
+				#print len(neighbours)
+				for x in neighbours:
+					if(goal ==1):
+						break
+					if (x.x == goalNode.x and x.y==goalNode.y):
+						goal=1
+						closedSet.add(x)
+					elif (checkClosedSet(x)):
+						checkOpenSet(x)
+
+					else:
+						continue
+		 		
+		reConstruct(startNode, goalNode,closedSet)
+	# find out the smallest node in openset
+	def smallestScore():
+
+		try:
+			test = next(iter(openSet))
+			minimum=test
+			for j in openSet:
+				#print j.f
+				if j.f < minimum.f:
+					minimum=j
+			return minimum
+		except StopIteration:
+			print "No more values to be iterated"
+	# construct path from closedset objects
+	def reConstruct(startNode, goalNode,closedSet):
+		idsch=-1
+		path=[]
+		current1=goalNode
+		path.append(goalNode)
+		for x in closedSet:
+			path.append(x)
+			for c in path:0
+			print '(' + str(c.x,) + str(c.y,)+')',
+		print "/ X,Y coordinates for found path"
+
+			
+		return path
 
 
 if __name__ == '__main__':
 
-    try:
-        goToPose()
-    except:
-    	rospy.loginfo("Terminated")
+	try:
+		goToPose()
+	except:
+		rospy.loginfo("Terminated !")
 
--- node.py
+++ node.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Odometry
+import numpy as np
+import math
 class node():
 #f, g, and h values for A* and Ids, locations
 	ids=0
@@ -9,7 +16,8 @@ class node():
 	h=0
 	status=0
 	parent=0
-	def __init__(self,x,y,g,h,f,status,parent):
+	def __init__(self,ids,x,y,g,h,f,status,parent):
+		self.ids=ids
 		self.x=x
 		self.y=y
 		self.g=g
@@ -17,3 +25,4 @@ class node():
 		self.f=f
 		self.status=status
 		self.parent=parent
+
